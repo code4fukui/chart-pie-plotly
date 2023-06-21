34ff05a7f0ff5a6192a8364e25145d26463220db
Taisuke Fukuno <fukuno@jig.jp>
2023-06-21 10:10:09 +0900

    use internal plotly.min.js

--- chart-pie.js
+++ chart-pie.js
@@ -1,4 +1,5 @@
-import "https://cdn.plot.ly/plotly-2.15.1.min.js";
+//import "https://cdn.plot.ly/plotly-2.15.1.min.js";
+import "./plotly.min.js";
 import { CSV } from "https://js.sabae.cc/CSV.js";
 //import { summarise } from "./summarise.js";
 
